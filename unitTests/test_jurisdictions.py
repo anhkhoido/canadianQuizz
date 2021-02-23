e78f9b74497e125054e94fdfb9e08b3ed711191c
anhkhoido <anhkhoido@gmail.com>
2021-02-22 19:50:11 -0500

    Added one setUp(self) method.

--- unitTests/test_jurisdictions.py
+++ unitTests/test_jurisdictions.py
@@ -3,12 +3,17 @@ from provinces_dictionary import us_states
 import unittest
 
 class TestJurisdiction(unittest.TestCase):
+    def setUp(self):
+        self.list_of_canadian_provinces_and_territories = list(canadian_provinces.keys())
+        self.list_of_american_states = list(us_states.keys())
+
     def test_correct_number_of_canadian_provinces(self):
-        list_canadian_provinces = list(canadian_provinces.values())
-        number = len(list_canadian_provinces)
+        number = len(self.list_of_canadian_provinces_and_territories)
         self.assertTrue(number == 13)
+
     def test_correct_number_of_american_states(self):
-        list_american_states = list(us_states.values())
-        number = len(list_american_states)
+        number = len(self.list_of_american_states)
         self.assertTrue(number == 50)
-unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
